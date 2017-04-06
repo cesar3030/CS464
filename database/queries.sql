2a03dd6b2a2555afc040c6a1719f394b9cc17e0f
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2017-04-05 21:40:23 -0600

    SELECT Queries: 2

--- database/queries.sql
+++ database/queries.sql
@@ -1,6 +1,10 @@
 -- SELECT queries
+
 -- Select the firstname and lastname of women without default address and
-SELECT firstname, lastname FROM Users WHERE default_address_id IS NULL AND sex = 'F'; 
+SELECT firstname, lastname FROM users WHERE default_address_id IS NULL AND sex = 'F'; 
+-- Select the products that have been linked to a deleted category
+SELECT * FROM products WHERE category_name IN (SELECT category_name FROM categories WHERE deleted_at IS NOT NULL)
+
 
 -- INSERT queries
 
