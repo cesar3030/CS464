8a7460fed33bfc50bf77e619651dd586c1f4b8b0
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2017-04-05 21:35:15 -0600

    project phase 3, write some SQL Queries

---
+++ database/queries.sql
@@ -0,0 +1,10 @@
+-- SELECT queries
+-- Select the firstname and lastname of women without default address and
+SELECT firstname, lastname FROM Users WHERE default_address_id IS NULL AND sex = 'F'; 
+
+-- INSERT queries
+
+-- DELETE queries
+
+-- UPDATE queries
+
