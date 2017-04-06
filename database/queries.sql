fb91a686da78cd2bf3f652532400844d15c379c8
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2017-04-05 23:07:12 -0600

    Added 1 INDEX

--- database/queries.sql
+++ database/queries.sql
@@ -1,17 +1,17 @@
 -- SELECT queries
 
 -- Select the firstname and lastname of user having an email with apache.org domain.
-SELECT firstname, lastname FROM users WHERE email REGEXP '.*@apache.org'
+SELECT firstname, lastname FROM users WHERE email REGEXP '.*@apache.org';
 -- Select the products that have been linked to a deleted category.
-SELECT * FROM products WHERE category_name IN (SELECT category_name FROM categories WHERE deleted_at IS NOT NULL)
+SELECT * FROM products WHERE category_name IN (SELECT category_name FROM categories WHERE deleted_at IS NOT NULL);
 -- Select the email of user which have submited an order, using a 3 days delivery type to an address that have its id > 86.
-SELECT DISTINCT o.email FROM items i
-NATURAL JOIN orders o
+SELECT DISTINCT o.email FROM orders o
+NATURAL JOIN items i
 NATURAL JOIN deliveries d
-WHERE d.address_id > 86
-AND d.type = '3 days';
+WHERE d.type = '3 days'
+AND i.quantity = 1;
 -- Count the number of product of each category and display the result from the category with the most products to the one with the least products.
-SELECT category_name, count(category_name) FROM products GROUP BY category_name ORDER BY count(category_name) DESC
+SELECT category_name, count(category_name) FROM products GROUP BY category_name ORDER BY count(category_name) DESC;
 -- Returns the list of products that have never been bought.
 SELECT DISTINCT name FROM products where product_id NOT IN (SELECT DISTINCT product_id FROM items);
 
@@ -41,3 +41,7 @@ UPDATE categories SET deleted_at = CURRENT_TIMESTAMP() - INTERVAL 10 WEEK WHERE 
 
 -- Deletes categories that have deleted_at set more than 60 days ago.
 DELETE FROM categories where TIMESTAMPDIFF(DAY, deleted_at, CURRENT_TIMESTAMP()) > 60;
+
+-- INDEXES
+CREATE INDEX product_name_index
+ON products (name);
