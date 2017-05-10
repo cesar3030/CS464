1f8347326256454f4cd1db7a9e8e9202fb68f457
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2017-05-10 12:39:55 -0700

    update git init to be able to transfer dist to UNM server

---
+++ server/config.php
@@ -0,0 +1,7 @@
+<?php
+// define (DB_USER, "admin");
+// define (DB_PASSWORD, "admin");
+define (DB_USER, "root");
+define (DB_PASSWORD, "root");
+define (DB_DATABASE, "am01780");
+define (DB_HOST, "localhost");
